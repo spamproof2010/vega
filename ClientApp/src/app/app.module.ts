d7f6e1a51280715f409c21e05c05af9a3847a2cc
unknown <reynald.adolphe@gmail.com>
2018-11-12 20:14:47 -0800

    Added New Compnent, route, bootstrap form, service & dropdown list

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { MakeService } from './Services/make.service';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { MakeService } from './Services/make.service';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
